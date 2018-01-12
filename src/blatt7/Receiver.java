edc03bb9209304cd00ea2217499bcb543bbd893f
Melanie <muell162@hm.edu>
2018-01-12 15:26:55 +0100

    added things, but not sure if working

--- src/blatt7/Receiver.java
+++ src/blatt7/Receiver.java
@@ -1,47 +1,135 @@
 package blatt7;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import blatt7.Packet;
+
+
+import java.io.IOException;
 
 public class Receiver {
+	
+	public static final String IP = "localhost";
+	public static final int PORT = 8001;
+	public static final int CHUNKSIZE = 1000; //Kb
+	public static final int TIME_LIMIT = 2000;
+	private BufferedReader reader;
+	private DatagramSocket socket;
+	private byte[] fileBytes;
+	private State currentState; 
+	private Transition[][] transition;
+	private InetAddress remoteIP;
+	private Packet ACK;
+	private Packet packet = null; 
+	
 	
 	enum State {
 		WAIT_0, WAIT_1
 	};
-	enum Msg {
-		// not sure yet 
+	enum Condition {
+		SEQ_0, SEQ_1, CORRUPT;
 	}; 
-	private State currentState; 
-	private Transition[][] transition; 
 	
-	public Receiver() {
+	
+	public Receiver() throws SocketException, UnknownHostException {
+		socket = new DatagramSocket(PORT);
+		socket.setSoTimeout(TIME_LIMIT);
+		remoteIP = Inet4Address.getByName(IP);
+		transition = new Transition[State.values().length][Condition.values().length];
 		
+		/* Conditions that allow the state machine to advance to the next state */
+		transition[State.WAIT_0.ordinal()]
+				[Condition.SEQ_0.ordinal()] = new SendACK0();
+		transition[State.WAIT_1.ordinal()]
+				[Condition.SEQ_1.ordinal()] = new SendACK1();
+		
+		/* Conditions that make the state machine repeat its last state */
+		transition[State.WAIT_1.ordinal()]
+				[Condition.SEQ_0.ordinal()] = new SendACK0();
+		transition[State.WAIT_0.ordinal()]
+				[Condition.SEQ_1.ordinal()] = new SendACK1();
+		transition[State.WAIT_1.ordinal()]
+				[Condition.CORRUPT.ordinal()] = new SendACK0();
+		transition[State.WAIT_0.ordinal()]
+				[Condition.CORRUPT.ordinal()] = new SendACK1();
 	}
+	
+	
 	
 	
 	abstract class Transition {
-		abstract public State execute();
+		abstract public State execute() throws IOException;
 	}
 	
-	class sendACK0 extends Transition {
+	class SendACK0 extends Transition {
 		@Override
-		public State execute() {
+		public State execute() throws IOException{
+			sendACK(0);
 			return State.WAIT_1;
 		}
 	}
 	
-	class sendACK1 extends Transition {
+	class SendACK1 extends Transition {
 		@Override
-		public State execute() {
+		public State execute() throws IOException{
+			sendACK(1);
 			return State.WAIT_0;
 		}
 	}
 	
-	public void wait(int seq) {
-		
+	private boolean processCondition(Condition cond) throws IOException {
+		Transition trans = transition[currentState.ordinal()]
+				[cond.ordinal()];
+		if(trans != null) {
+			currentState = trans.execute();
+			return true;
+		} else {
+			return false;
+		}
+	}
+	private Condition getCondition(Packet packet) {
+			if (packet.getSeq() == 0) {
+				return Condition.SEQ_0;
+			} else {
+				return Condition.SEQ_1;
+			}
 	}
 	
-	private String getSeq(Packet p) {
-		//extract Seq Number 
-		return null;
+	private void sendACK(int i) throws IOException {
+		//send ACK as packet, int either 0 or 1 
+		DatagramPacket p = new DatagramPacket(ACK.getBytes(), ACK.length());
+		socket.send(p);
+	}
+	
+	private void receivePacket() throws IOException {
+		byte buffer[] = new byte[1024];
+		boolean success = false; 
+		while(!success) {
+			DatagramPacket p = new DatagramPacket(buffer, buffer.length);
+			try {
+				socket.receive(p);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			packet = new Packet(p.getData());
+			File file = new File("C:\\Users\\Melanie\\Desktop\\Packet.txt");
+			try (FileOutputStream fos = new FileOutputStream("myfile.txt", true)) {
+				   fos.write(packet.getBytes());
+				   fos.close();
+				}
+			success = processCondition(getCondition(packet));
+		}
+		
 	}
 
 }
